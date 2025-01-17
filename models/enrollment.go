02e93ae1da47262197fe7f7eff9d04e10a46fa80
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-17 13:12:30 +0530

    models: enrollment

---
+++ models/enrollment.go
@@ -0,0 +1,21 @@
+/**
+ * @file: enrollment.go
+ * @author: Krisna Pranav
+ * @brief: enrollment
+ * @version 1.0
+ * @date 2025-01-17
+ *
+ * @copyright Copyright (c) 2024-2025 Krisna Pranav
+ *
+ */
+
+package models
+
+import "time"
+
+type Enrollment struct {
+	ID        uint      `json:"id" gorm:"primaryKey"`
+	UserID    uint      `json:"user_id"`
+	CourseID  uint      `json:"course_id"`
+	CreatedAt time.Time `json:"created_at"`
+}
