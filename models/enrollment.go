723ea2cbd8fb591b71612abc9c23f03a815a864a
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-17 13:12:50 +0530

    models: enrollment

--- models/enrollment.go
+++ models/enrollment.go
@@ -14,8 +14,9 @@ package models
 import "time"
 
 type Enrollment struct {
-	ID        uint      `json:"id" gorm:"primaryKey"`
+	ID        uint      `json:"id" gorm:"primaryKey;autoIncrement"`
 	UserID    uint      `json:"user_id"`
 	CourseID  uint      `json:"course_id"`
+	Completed bool      `json:"completed"`
 	CreatedAt time.Time `json:"created_at"`
 }
